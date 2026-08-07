e3a5af637862cf341ebb8322ed943edb8ae32221
Zubaire404 <mass62028@gmail.com>
2026-08-07 16:02:19 +0600

    Updated default seed data with Bogura routes

--- setup_schedules.py
+++ setup_schedules.py
@@ -8,20 +8,24 @@ def setup():
 
     print("Adding default bus schedules with fares...")
     
-    # Dhaka - Sylhet (Base Fare: BDT 800)
-    database.add_bus_schedule("Bus-1", "Dhaka - Sylhet", "10:00 AM", base_fare=800)
-    database.add_bus_schedule("Bus-2", "Dhaka - Sylhet", "12:00 PM", base_fare=800)
-    database.add_bus_schedule("Bus-3", "Dhaka - Sylhet", "08:00 PM", base_fare=800)
+    # Bogura - Dhaka (Base Fare: BDT 700)
+    database.add_bus_schedule("SR Travels (Non-AC)", "Bogura - Dhaka", "08:30 AM", base_fare=700)
+    database.add_bus_schedule("Shyamoli Paribahan (AC)", "Bogura - Dhaka", "10:00 AM", base_fare=1200)
+    database.add_bus_schedule("Hanif Enterprise (AC)", "Bogura - Dhaka", "11:30 PM", base_fare=1200)
 
-    # Dhaka - Cox's Bazar (Base Fare: BDT 1600)
-    database.add_bus_schedule("Bus-1", "Dhaka - Cox's Bazar", "08:00 AM", base_fare=1600)
-    database.add_bus_schedule("Bus-2", "Dhaka - Cox's Bazar", "10:00 AM", base_fare=1600)
-    database.add_bus_schedule("Bus-3", "Dhaka - Cox's Bazar", "09:00 PM", base_fare=1600)
+    # Bogura - Chittagong (Base Fare: BDT 1400)
+    database.add_bus_schedule("Ena Transport (Non-AC)", "Bogura - Chittagong", "07:00 PM", base_fare=1400)
+    database.add_bus_schedule("Hanif Enterprise (AC)", "Bogura - Chittagong", "08:30 PM", base_fare=1800)
+    database.add_bus_schedule("Desh Travels (AC)", "Bogura - Chittagong", "09:30 PM", base_fare=1800)
 
-    # Dhaka - Chittagong (Base Fare: BDT 900)
-    database.add_bus_schedule("Bus-1", "Dhaka - Chittagong", "07:30 AM", base_fare=900)
-    database.add_bus_schedule("Bus-2", "Dhaka - Chittagong", "11:00 AM", base_fare=900)
-    database.add_bus_schedule("Bus-3", "Dhaka - Chittagong", "11:30 PM", base_fare=900)
+    # Bogura - Cox's Bazar (Base Fare: BDT 1800)
+    database.add_bus_schedule("Shyamoli Paribahan (Non-AC)", "Bogura - Cox's Bazar", "05:30 PM", base_fare=1800)
+    database.add_bus_schedule("Nabil Paribahan (AC)", "Bogura - Cox's Bazar", "07:30 PM", base_fare=2200)
+    database.add_bus_schedule("Hanif Enterprise (AC)", "Bogura - Cox's Bazar", "08:45 PM", base_fare=2200)
+    
+    # Bogura - Rajshahi (Base Fare: BDT 300)
+    database.add_bus_schedule("Desh Travels (Non-AC)", "Bogura - Rajshahi", "08:00 AM", base_fare=300)
+    database.add_bus_schedule("Hanif Enterprise (Non-AC)", "Bogura - Rajshahi", "02:00 PM", base_fare=300)
 
     print("Bus schedules with fares added successfully!")
 
